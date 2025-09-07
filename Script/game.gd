79b21e9aec37a4d4fa4a9e213aa46990ef6b4bb0
theludovyc <theludovyc@gmail.com>
2025-09-07 17:56:07 +0200

    update

--- Script/game.gd
+++ Script/game.gd
@@ -1,6 +1,9 @@
 extends Node
 
-@onready var ui = $UI
+@onready var ui:UI = $UI
 
 func _on_dead_line_still_in_collision_with_ball() -> void:
-	prints(name, "Game over")
+	ui.setGameOver(true)
+
+func _on_retry_button_button_down() -> void:
+	get_tree().reload_current_scene()
--- Script/ui.gd
+++ Script/ui.gd
@@ -1,6 +1,13 @@
 extends Control
+class_name UI
 
 @onready var scoreLabel:Label = $ScoreLabel
+@onready var occludeRect:ColorRect = $OccludeRect
+@onready var gameOverLabel:Label = $GameOverLabel
 
 func setScore(i:int):
 	scoreLabel.text = str(i)
+
+func setGameOver(b:bool):
+	occludeRect.visible = b
+	gameOverLabel.visible = b
