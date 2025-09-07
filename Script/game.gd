b2178ad325d373c6d61c8b0698c0cb119bf51234
theludovyc <theludovyc@gmail.com>
2025-09-07 18:33:29 +0200

    update

--- Script/game.gd
+++ Script/game.gd
@@ -1,8 +1,23 @@
 extends Node
 
 @onready var ui:UI = $UI
+@onready var spawner:Spawner = %Spawner
+@onready var spawnTimer:Timer = $SpawnTimer
 
 var score := 0
+
+var next_radius : int
+
+func _ready() -> void:
+	next_radius = randi_range(0, 3)
+	
+	spawner.spawn_ball(next_radius)
+	
+func _process(_delta: float) -> void:
+	if spawnTimer.is_stopped() and Input.is_action_just_pressed("ui_accept"):
+		spawner.release_ball()
+		
+		spawnTimer.start()
 
 func _on_dead_line_still_in_collision_with_ball() -> void:
 	ui.setGameOver(true)
@@ -14,3 +29,8 @@ func _on_ball_evolved(radius:int) -> void:
 	score += radius
 	
 	ui.setScore(score)
+
+func _on_spawn_timer_timeout() -> void:
+	next_radius = randi_range(0, 3)
+	
+	spawner.spawn_ball(next_radius)
--- Script/node_2d.gd
+++ Script/node_2d.gd
@@ -1,51 +1,32 @@
 extends Node2D
+class_name Spawner
 
 @export var Balls : Node2D
 
-@export var colors_grad : GradientTexture1D
-
-@onready var timer : Timer = $Timer
-
-const Speed = 100
-
 var Ball = preload("res://scene/ball.tscn")
 
-var next_radius : int
-
 var ball : Node2D
-
-func spawn_ball():
-	ball = Ball.instantiate()
-	
-	add_child(ball)
-	
-	ball.set_radius(next_radius)
 
 func _ready() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(get_parent(), "progress_ratio", 1, 2)
 	tween.chain().tween_property(get_parent(), "progress_ratio", 0, 2)
 	tween.set_loops()
-	
-	next_radius = randi_range(0, 3)
-	
-	spawn_ball()
 
-func _process(delta: float) -> void:
-	if timer.is_stopped() and Input.is_action_just_pressed("ui_accept"):
-		remove_child(ball)
-		
-		ball.position = global_position
-		
-		ball.connect("evolved", get_tree().current_scene._on_ball_evolved)
-		
-		Balls.add_child(ball)
-		
-		ball.process_mode = Node.PROCESS_MODE_INHERIT
-		
-		next_radius = randi_range(0, 3)
+func spawn_ball(radius:int):
+	ball = Ball.instantiate()
 	
-		timer.start()
+	add_child(ball)
+	
+	ball.set_radius(radius)
 
-func _on_timer_timeout() -> void:
-	spawn_ball()
+func release_ball():
+	remove_child(ball)
+		
+	ball.position = global_position
+	
+	ball.connect("evolved", get_tree().current_scene._on_ball_evolved)
+	
+	Balls.add_child(ball)
+	
+	ball.process_mode = Node.PROCESS_MODE_INHERIT
