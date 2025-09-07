5c8af9f2eb6a3a3c648ee516b082910555befb87
theludovyc <theludovyc@gmail.com>
2025-09-07 20:47:37 +0200

    update

--- Script/game.gd
+++ Script/game.gd
@@ -8,18 +8,22 @@ var score := 0
 
 var next_radius : int
 
+var gameOver := false
+
 func _ready() -> void:
 	next_radius = randi_range(0, 3)
 	
 	spawner.spawn_ball(next_radius)
 	
 func _process(_delta: float) -> void:
-	if spawnTimer.is_stopped() and Input.is_action_just_pressed("ui_accept"):
+	if not gameOver and spawnTimer.is_stopped() and Input.is_action_just_pressed("ui_accept"):
 		spawner.release_ball()
 		
 		spawnTimer.start()
 
 func _on_dead_line_still_in_collision_with_ball() -> void:
+	gameOver = true
+	
 	ui.setGameOver(true)
 
 func _on_retry_button_button_down() -> void:
@@ -31,6 +35,9 @@ func _on_ball_evolved(radius:int) -> void:
 	ui.setScore(score)
 
 func _on_spawn_timer_timeout() -> void:
+	if gameOver:
+		return
+	
 	next_radius = randi_range(0, 3)
 	
 	spawner.spawn_ball(next_radius)
